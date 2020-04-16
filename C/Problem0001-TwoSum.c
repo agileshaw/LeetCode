c09463b9074e41b9207b7ee2548062431603d891
agileshaw <agileshaw@gmail.com>
2020-04-15 22:56:18 -0400

    Update C Solution for Problem0001

--- C/Problem0001-TwoSum.c
+++ C/Problem0001-TwoSum.c
@@ -1,37 +1,41 @@
 /**
  * Problem001 - Two Sum
- * 
+ *
  * https://github.com/agileshaw/LeetCode
  **/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int* twoSum(int* nums, int numsSize, int target)
-{
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     int i, j;
-    int *suc = (int*)malloc(sizeof(int) * 2);
-    
-    for (i = 0; i < numsSize; i++) {
-        for (j = i+1; j < numsSize; j++) {
+    int *array = malloc(sizeof(int)*2);
+
+    *returnSize = 2;
+    for (i = 0; i < numsSize-1; i++) {
+        for(j = i+1; j < numsSize; j++) {
             if (nums[i] + nums[j] == target) {
-                suc[0] = i;
-                suc[1] = j;
+                array[0] = i;
+                array[1] = j;
+                break;
             }
         }
     }
-    
-    return suc;
+
+    return array;
 }
 
 int main()
 {
 	int num[] = {2, 7, 11, 15};
-	int target = 9;
+	int target = 9, returnSize, i;
 
-	int *index = twoSum(&num[0], 4, target);
+	int *index = twoSum(&num[0], 4, target, &returnSize);
 
-	printf("[%d, %d]\n", index[0], index[1]);
+    printf("[%d", index[0]);
+    for (i = 1; i < returnSize; i++)
+        printf(",%d", index[i]);
+	printf("]\n");
 
 	free(index);
 
